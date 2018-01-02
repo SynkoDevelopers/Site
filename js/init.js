63dc3cefd9263cf89f48ff273d8401fbaf67f398
Synk0 <wesleyhod.wh@gmail.com>
2018-01-02 16:57:27 -0200

    JQuery New

--- js/init.js
+++ js/init.js
@@ -1,8 +1,4 @@
-$(function(){
-	$(function() {
-
-		$(".button-collapse").sideNav();
-		$(".parallax").parallax();
-
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
+$(function() {
+	$(".button-collapse").sideNav();
+	$(".parallax").parallax();
+});
