e068756eadd2c1d8a75290a7bad47074dc09215d
Synk0 <wesleyhod.wh@gmail.com>
2018-01-02 16:38:40 -0200

    Strings must use doublequote.

--- js/top.js
+++ js/top.js
@@ -2,14 +2,14 @@ $(function(){
     
     $(window).scroll(function(){
         if ($(this).scrollTop() > 632) {
-            $('.fixed-action-btn').fadeIn();
+            $(".fixed-action-btn").fadeIn();
         } else {
-            $('.fixed-action-btn').fadeOut();
+            $(".fixed-action-btn").fadeOut();
         }
     });
 
-    $('.fixed-action-btn').click(function(){
-        $('html, body').animate({scrollTop : 0},800);
+    $(".fixed-action-btn").click(function(){
+        $("html, body").animate({scrollTop : 0},800);
         return false;
     });
 });
