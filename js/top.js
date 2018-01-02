64d1a9bc90aa47168d07673c4d33270e88182d47
Synk0 <wesleyhod.wh@gmail.com>
2018-01-02 16:34:18 -0200

    Criando botão FAB

---
+++ js/top.js
@@ -0,0 +1,15 @@
+$(function(){
+    
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 632) {
+            $('.fixed-action-btn').fadeIn();
+        } else {
+            $('.fixed-action-btn').fadeOut();
+        }
+    });
+
+    $('.fixed-action-btn').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+});
